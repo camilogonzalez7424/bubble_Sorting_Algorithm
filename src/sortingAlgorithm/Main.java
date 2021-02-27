0b1aae0b6f1caab3b1b12a52bcd981fb2d5564d1
camilogonzalez7424 <gonzalezcamilo508@gmail.com>
2021-02-26 21:06:48 -0500

    correct reading

--- src/sortingAlgorithm/Main.java
+++ src/sortingAlgorithm/Main.java
@@ -7,9 +7,24 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
+    public final static String separator = (" ");
+
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int size = Integer.parseInt(br.readLine());
+        String[] row = new String[size];
+        String chain = "";
+        while (size>0) {
+            chain = br.readLine();
+            row[size-1]= chain;
+            size--;
+          //System.out.println(row[size]);
+        }
+
+
+
 
         br.close();
         bw.close();
