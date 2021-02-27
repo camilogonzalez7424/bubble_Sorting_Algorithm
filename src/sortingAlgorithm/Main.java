668567aa9ed7597b86a068d77a671d6d2be089f0
camilogonzalez7424 <gonzalezcamilo508@gmail.com>
2021-02-26 23:42:20 -0500

    clasificación implementada

--- src/sortingAlgorithm/Main.java
+++ src/sortingAlgorithm/Main.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main {
-    public final static String separator = (" ");
+    public final static String separator = (" "); //Constant used to separate.
 
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,28 +15,65 @@ public class Main {
 
         int size = Integer.parseInt(br.readLine());
         String[] row = new String[size];
-        String chain = "";
+        String chain;
         while (size>0) {
             chain = br.readLine();
             row[size-1]= chain;
             size--;
           //System.out.println(row[size]);
+            bw.write(bubbleSort(split(row[size])));
         }
-
         br.close();
         bw.close();
-
-
     }
+
+    //method that separates the strings.
     public static double[] split(String chain) {
         String[] parts = chain.split(separator);
         double[] array = new double[parts.length];
         for(int i = 0; i < array.length; i++) {
-            array[i] = Double.parseDouble(parts[i]);
-
+            array[i] = Double.parseDouble(parts[i]); //pass from string to double.
         }
 
         return array;
+    }
+    //Bubble sort method
+    public static String bubbleSort(double[] array) {
+        boolean changed = true;
+        double count = 0;
+        String text;
+        for (int i = 1; i < array.length && changed; i++) {
+            changed = false;
+            for(int j = 0; j < array.length-1; j++) {
+                if(array[j]>array[j+1]) {
+                    count++;
+                    double temp   = array[j];
+                    array[j]   = array[j+1];
+                    array[j+1] = temp;
+                    changed = true;
+                }
+
+            }
+        }
+        boolean out = true;
+        String space = "";
+        for(int i = 0; i < array.length ; i++){
+            if(!out){
+                space+=" ";
+            }
+            space+=array[i];
+            out = false;
+        }
+
+        double average = 0;
+        if(array.length == 1) {
+            average = count;
+        } else {
+            average = count/(array.length-1);
+        }
+        double averageFinal = Math.floor(average*100)/100; //two decimal places.
+        text = averageFinal+"-"+space+"\n";
+        return text;
     }
 
 }
